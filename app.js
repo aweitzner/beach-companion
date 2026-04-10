f0eeee6a5cb93765095594f6e11f927abe435184
Andrew Weitzner <a.weitzner@gmail.com>
2026-04-10 16:44:58 -0400

    iphone pic too tall

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const APP_VERSION = 'v1.5.6';
+const APP_VERSION = 'v1.5.7';
 const queryParams = new URLSearchParams(window.location.search);
 const TEST_MODE = queryParams.get('testMode') === '1';
 const TEST_MODE_CONFIG = Object.freeze({
