a86d37c1a11f25fb8e59b1769857e3e8b7acc760
Andrew Weitzner <a.weitzner@gmail.com>
2026-04-10 20:08:57 -0400

    higher more

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const APP_VERSION = 'v1.5.11';
+const APP_VERSION = 'v1.5.13';
 const queryParams = new URLSearchParams(window.location.search);
 const TEST_MODE = queryParams.get('testMode') === '1';
 const TEST_MODE_CONFIG = Object.freeze({
